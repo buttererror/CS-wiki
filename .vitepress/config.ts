c29cacf5152830d7a58c669ea069f08b41b96d96
Mahmoud <mahmoud@buttererror.com>
2026-08-06 18:05:17 +0300

    feat: add VitePress wiki build foundation

---
+++ .vitepress/config.ts
@@ -0,0 +1,397 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+    title: 'CS Wiki',
+    description: "A practical, evolving knowledge base for computer science and software development.",
+    base: "/",
+    cleanUrls: true,
+    srcExclude: ['docs/**', 'README.md', 'AGENTS.md'],
+    rewrites: (id) => {
+        if (id.endsWith('/README.md')) {
+            return id.replace(/README\.md$/, 'index.md')
+        }
+
+        return id
+    },
+
+    themeConfig: {
+        siteTitle: "CS Wiki",
+        search: {
+            provider: 'local',
+
+        },
+        outline: [2, 3],
+
+        nav: [
+            {
+                text: 'Foundations',
+                link: '/computer-science-foundations/',
+            },
+            {
+                text: 'System Design',
+                link: '/system-design/',
+            },
+            {
+                text: 'Security',
+                link: '/security/',
+            },
+            {
+                text: 'Practices',
+                link: '/software-development-practices/',
+            },
+            {
+                text: 'Frontend',
+                link: '/frontend-development/',
+            },
+            {
+                text: 'Tooling',
+                link: '/framework-tooling/',
+            },
+        ],
+        sidebar: {
+            '/computer-science-foundations/': [
+                {
+                    text: 'Computer Science Foundations',
+                    link: '/computer-science-foundations/',
+                    items: [
+                        {
+                            text: 'Computer Systems',
+                            link: '/computer-science-foundations/computer-systems/',
+                        },
+                        {
+                            text: 'Software Engineering',
+                            link: '/computer-science-foundations/software-engineering/',
+                            collapsed: false,
+                            items: [
+                                {
+                                    text: 'Software Taxonomy',
+                                    link: '/computer-science-foundations/software-engineering/software-taxonomy',
+                                },
+                                {
+                                    text: 'Software Architecture',
+                                    link: '/computer-science-foundations/software-engineering/software-architecture',
+                                },
+                                {
+                                    text: 'Programming Paradigms',
+                                    link: '/computer-science-foundations/software-engineering/programming-paradigms/',
+                                },
+                                {
+                                    text: 'Software Design Principles',
+                                    link: '/computer-science-foundations/software-engineering/software-design-principles/',
+                                },
+                                {
+                                    text: 'Terminology',
+                                    link: '/computer-science-foundations/software-engineering/terminology/',
+                                },
+                                {
+                                    text: 'Communication Patterns',
+                                    link: '/computer-science-foundations/software-engineering/communication-patterns/',
+                                },
+                                {
+                                    text: 'Design Patterns',
+                                    link: '/computer-science-foundations/software-engineering/design-patterns/',
+                                },
+                                {
+                                    text: 'Architectural Patterns',
+                                    link: '/computer-science-foundations/software-engineering/architectural-patterns/',
+                                },
+                                {
+                                    text: 'Architectural Styles',
+                                    link: '/computer-science-foundations/software-engineering/architectural-styles/',
+                                    items: [
+                                        {
+                                            text: 'Event-Driven Architecture',
+                                            link: '/computer-science-foundations/software-engineering/architectural-styles/event-driven-architecture',
+                                        },
+                                        {
+                                            text: 'Microservice Architecture',
+                                            link: '/computer-science-foundations/software-engineering/architectural-styles/microservice-architecture',
+                                        },
+                                        {
+                                            text: 'Modular Monolith',
+                                            link: '/computer-science-foundations/software-engineering/architectural-styles/modular-monolith',
+                                        },
+                                    ],
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+
+            '/system-design/': [
+                {
+                    text: 'System Design',
+                    link: '/system-design/',
+                    items: [
+                        {
+                            text: 'Software System Design',
+                            link: '/system-design/software-system-design/',
+                            items: [
+                                {
+                                    text: 'Domain-Driven Design',
+                                    link: '/system-design/software-system-design/domain-driven-design',
+                                },
+                                {
+                                    text: 'Neighboring Perspectives',
+                                    link: '/system-design/software-system-design/neighboring-perspectives',
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+
+            '/security/': [
+                {
+                    text: 'Security',
+                    link: '/security/',
+                    items: [
+                        {
+                            text: 'Identity and Access Management',
+                            link: '/security/identity-and-access-management/',
+                            items: [
+                                {
+                                    text: 'Authentication',
+                                    link: '/security/identity-and-access-management/authentication',
+                                },
+                                {
+                                    text: 'JSON Web Token',
+                                    link: '/security/identity-and-access-management/json-web-token',
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+
+            '/software-development-practices/': [
+                {
+                    text: 'Software Development Practices',
+                    link: '/software-development-practices/',
+                    items: [
+                        {
+                            text: 'Development Strategy',
+                            link: '/software-development-practices/development-strategy',
+                        },
+                        {
+                            text: 'Lean MVP and Vertical Slices',
+                            link: '/software-development-practices/lean-mvp-vertical-slice-development',
+                        },
+                        {
+                            text: 'Repository Organization',
+                            link: '/software-development-practices/repository-organization/',
+                            items: [
+                                {
+                                    text: 'Monorepo',
+                                    link: '/software-development-practices/repository-organization/monorepo',
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+
+            '/frontend-development/': [
+                {
+                    text: 'Frontend Development',
+                    link: '/frontend-development/',
+                    items: [
+                        {
+                            text: 'Browser Runtime',
+                            link: '/frontend-development/browser-runtime/',
+                        },
+                        {
+                            text: 'Rendering',
+                            link: '/frontend-development/rendering/',
+                            items: [
+                                {
+                                    text: 'Server and Client Rendering',
+                                    link: '/frontend-development/rendering/server-and-client-rendering',
+                                },
+                                {
+                                    text: 'Hydration',
+                                    link: '/frontend-development/rendering/hydration',
+                                },
+                            ],
+                        },
+                        {
+                            text: 'Data Across Boundaries',
+                            items: [
+                                {
+                                    text: 'Serialization',
+                                    link: '/frontend-development/data-across-boundaries/serialization',
+                                },
+                            ],
+                        },
+                        {
+                            text: 'State and Reactivity',
+                            items: [
+                                {
+                                    text: 'Reactivity Mechanisms',
+                                    link: '/frontend-development/state-and-reactivity/reactivity-mechanisms',
+                                },
+                            ],
+                        },
+                        {
+                            text: 'Styling',
+                            link: '/frontend-development/styling/',
+                        },
+                    ],
+                },
+            ],
+
+            '/framework-tooling/': [
+                {
+                    text: 'Frameworks, Libraries, and Tooling',
+                    link: '/framework-tooling/',
+                    items: [
+                        {
+                            text: 'NestJS Dependency Injection',
+                            link: '/framework-tooling/nestjs-dependency-injection',
+                        },
+                        {
+                            text: 'TanStack Query',
+                            link: '/framework-tooling/tanstack-query',
+                        },
+                        {
+                            text: 'Frontend',
+                            link: '/framework-tooling/frontend/',
+                            items: [
+                                {
+                                    text: 'React',
+                                    link: '/framework-tooling/frontend/react/',
+                                    items: [
+                                        {
+                                            text: 'State and Updates',
+                                            link: '/framework-tooling/frontend/react/state-and-updates',
+                                        },
+                                        {
+                                            text: 'Rendering Model',
+                                            link: '/framework-tooling/frontend/react/rendering-model',
+                                        },
+                                        {
+                                            text: 'Context and External Stores',
+                                            link: '/framework-tooling/frontend/react/context-and-external-stores',
+                                        },
+                                        {
+                                            text: 'Effects and External Synchronization',
+                                            link: '/framework-tooling/frontend/react/effects-and-external-synchronization',
+                                        },
+                                        {
+                                            text: 'Forms',
+                                            link: '/framework-tooling/frontend/react/forms',
+                                        },
+                                        {
+                                            text: 'Performance',
+                                            link: '/framework-tooling/frontend/react/performance',
+                                        },
+                                    ],
+                                },
+                                {
+                                    text: 'React Application Delivery',
+                                    link: '/framework-tooling/frontend/react-application-delivery/',
+                                    items: [
+                                        {
+                                            text: 'Next.js vs React with Vite',
+                                            link: '/framework-tooling/frontend/react-application-delivery/nextjs-vs-react-with-vite',
+                                        },
+                                    ],
+                                },
+                                {
+                                    text: 'Next.js',
+                                    link: '/framework-tooling/frontend/nextjs/notes',
+                                },
+                                {
+                                    text: 'Vue',
+                                    link: '/framework-tooling/frontend/vue/notes',
+                                },
+                            ],
+                        },
+                        {
+                            text: 'Cross-Platform UI',
+                            link: '/framework-tooling/cross-platform-ui/',
+                            items: [
+                                {
+                                    text: 'Flutter',
+                                    link: '/framework-tooling/cross-platform-ui/flutter',
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+
+            '/miscellaneous/': [
+                {
+                    text: 'Miscellaneous Notes',
+                    link: '/miscellaneous/',
+                    items: [
+                        {
+                            text: 'General Programming',
+                            link: '/miscellaneous/CS',
+                        },
+                        {
+                            text: 'JavaScript',
+                            link: '/miscellaneous/JavaScript-notes',
+                        },
+                        {
+                            text: 'TypeScript',
+                            link: '/miscellaneous/TypeScript',
+                        },
+                        {
+                            text: 'General Notes',
+                            link: '/miscellaneous/general',
+                        },
+                        {
+                            text: 'Technical Fixes and Solutions',
+                            link: '/miscellaneous/technical-fixes-and-solutions',
+                        },
+                        {
+                            text: 'Git',
+                            link: '/miscellaneous/git-space',
+                        },
+                        {
+                            text: 'AI',
+                            items: [
+                                {
+                                    text: 'LLMs vs. AI Agents',
+                                    link: '/miscellaneous/ai/llm-vs-ai-agent',
+                                },
+                            ],
+                        },
+                        {
+                            text: 'Backend',
+                            items: [
+                                {
+                                    text: 'Databases',
+                                    link: '/miscellaneous/backend/databases',
+                                },
+                            ],
+                        },
+                        {
+                            text: 'Linux',
+                            items: [
+                                {
+                                    text: 'Linux General',
+                                    link: '/miscellaneous/linux/linux-general',
+                                },
+                                {
+                                    text: 'Docker',
+                                    link: '/miscellaneous/linux/docker',
+                                },
+                                {
+                                    text: 'PostgreSQL',
+                                    link: '/miscellaneous/linux/postgresql',
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+        }
+
+
+    },
+
+})
